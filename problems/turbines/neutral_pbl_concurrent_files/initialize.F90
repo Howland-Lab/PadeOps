06401ebb0fe25d4da11258c25f67eb1e395d81c3
Kirby Heck <kirby.heck@gmail.com>
2024-05-21 16:02:28 -0500

    removed unused variables and whitespace

--- problems/turbines/neutral_pbl_concurrent_files/initialize.F90
+++ problems/turbines/neutral_pbl_concurrent_files/initialize.F90
@@ -1,21 +1,21 @@
 module neutral_pbl_concurrent_parameters
 
-      ! TAKE CARE OF TIME NON-DIMENSIONALIZATION IN THIS MODULE
+    ! TAKE CARE OF TIME NON-DIMENSIONALIZATION IN THIS MODULE
 
     use exits, only: message
     use kind_parameters,  only: rkind
-    use constants, only: zero, kappa, pi 
+    use constants, only: zero, kappa, pi
     implicit none
     integer :: seedu = 321341
     integer :: seedv = 423424
     integer :: seedw = 131344
     real(rkind) :: randomScaleFact = 0.002_rkind ! 0.2% of the mean value
     integer :: nxg, nyg, nzg
-    
+
     real(rkind), parameter :: xdim = 400._rkind, udim =5._rkind
     real(rkind), parameter :: timeDim = xdim/udim
 
-end module     
+end module
 
 
 subroutine initfields_wallM(decompC, decompE, inputfile, mesh, fieldsC, fieldsE)
@@ -24,7 +24,7 @@ subroutine initfields_wallM(decompC, decompE, inputfile, mesh, fieldsC, fieldsE)
     use constants,          only: zero, one, two, pi, half
     use gridtools,          only: alloc_buffs
     use random,             only: gaussian_random
-    use decomp_2d          
+    use decomp_2d
     use reductions,         only: p_maxval
     use constants,          only: pi
     implicit none
@@ -39,10 +39,10 @@ subroutine initfields_wallM(decompC, decompE, inputfile, mesh, fieldsC, fieldsE)
     real(rkind), dimension(:,:,:), allocatable :: ybuffC, ybuffE, zbuffC, zbuffE, ztmp
     integer :: nz, nzE, k
     real(rkind) :: sig
-    real(rkind)  :: Lx = one, Ly = one, Lz = one, Tref = zero, Tsurf0 = one, dTsurf_dt = -0.05d0, z0init = 1.d-4, frameAngle = -26.d0 
-    real(rkind), dimension(:,:,:), allocatable :: randArr, Tpurt, eta
-    
-    namelist /PROBLEM_INPUT/ Lx, Ly, Lz, Tref, Tsurf0, dTsurf_dt, z0init, frameAngle 
+    real(rkind)  :: Lx = one, Ly = one, Lz = one, Tref = zero, Tsurf0 = one, dTsurf_dt = -0.05d0, z0init = 1.d-4, frameAngle = -26.d0
+    real(rkind), dimension(:,:,:), allocatable :: randArr, Tpurt
+
+    namelist /PROBLEM_INPUT/ Lx, Ly, Lz, Tref, Tsurf0, dTsurf_dt, z0init, frameAngle
     !real(rkind)  :: beta, sigma, phi_ref
     !integer :: z_ref
     !namelist /PROBLEM_INPUT/ Lx, Ly, Lz, Tref, Tsurf0, dTsurf_dt, z0init, frameAngle!, beta, sigma, phi_ref, z_ref
@@ -50,12 +50,12 @@ subroutine initfields_wallM(decompC, decompE, inputfile, mesh, fieldsC, fieldsE)
     ioUnit = 11
     open(unit=ioUnit, file=trim(inputfile), form='FORMATTED')
     read(unit=ioUnit, NML=PROBLEM_INPUT)
-    close(ioUnit)    
+    close(ioUnit)
 
 
     !!!!!!!!!!!!!!!!!!!!! DON'T CHANGE THE POINTERS / ALLOCATIONS !!!!!!!!!!!!!!!!!!!!!!
     u  => fieldsC(:,:,:,1); v  => fieldsC(:,:,:,2); wC => fieldsC(:,:,:,3)
-    w  => fieldsE(:,:,:,1); T  => fieldsC(:,:,:,7) 
+    w  => fieldsE(:,:,:,1); T  => fieldsC(:,:,:,7)
     z => mesh(:,:,:,3); y => mesh(:,:,:,2); x => mesh(:,:,:,1)
     !allocate(Tpurt(decompC%xsz(1),decompC%xsz(2),decompC%xsz(3)))
     !allocate(randArr(size(T,1),size(T,2),size(T,3)))
@@ -65,9 +65,9 @@ subroutine initfields_wallM(decompC, decompE, inputfile, mesh, fieldsC, fieldsE)
     u = one
     v = zero
     wC = zero
-    ! Added to account for frame angle    
+    ! Added to account for frame angle
     u = u * cos(frameAngle * pi / 180.d0)
-    v = v * sin(frameAngle * pi / 180.d0) 
+    v = v * sin(frameAngle * pi / 180.d0)
 
     allocate(ztmp(decompC%xsz(1),decompC%xsz(2),decompC%xsz(3)))
     allocate(Tpurt(decompC%xsz(1),decompC%xsz(2),decompC%xsz(3)))
@@ -108,9 +108,9 @@ subroutine initfields_wallM(decompC, decompE, inputfile, mesh, fieldsC, fieldsE)
     zbuffE = zero
     zbuffE(:,:,2:nzE-1) = half*(zbuffC(:,:,1:nz-1) + zbuffC(:,:,2:nz))
     call transpose_z_to_y(zbuffE,ybuffE,decompE)
-    call transpose_y_to_x(ybuffE,w,decompE) 
-    ! Deallocate local memory 
-    deallocate(ybuffC,ybuffE,zbuffC, zbuffE) 
+    call transpose_y_to_x(ybuffE,w,decompE)
+    ! Deallocate local memory
+    deallocate(ybuffC,ybuffE,zbuffC, zbuffE)
     nullify(u,v,w,x,y,z)
     call message(0,"Velocity Field Initialized")
     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -120,19 +120,19 @@ end subroutine
 
 subroutine setInhomogeneousNeumannBC_Temp(inputfile, wTh_surf)
     use kind_parameters,    only: rkind
-    use constants, only: one, zero 
+    use constants, only: one, zero
     implicit none
 
     character(len=*),                intent(in)    :: inputfile
     real(rkind), intent(out) :: wTh_surf
-    integer :: ioUnit 
-    real(rkind) :: wt_surface 
-    namelist /BOUNDARY_FLUX/wt_surface 
-     
+    integer :: ioUnit
+    real(rkind) :: wt_surface
+    namelist /BOUNDARY_FLUX/wt_surface
+
     ioUnit = 11
     open(unit=ioUnit, file=trim(inputfile), form='FORMATTED')
     read(unit=ioUnit, NML=BOUNDARY_FLUX)
-    close(ioUnit)    
+    close(ioUnit)
 
     wTh_surf = wt_surface
 
@@ -143,26 +143,26 @@ end subroutine
 subroutine setDirichletBC_Temp(inputfile, Tsurf, dTsurf_dt)
     use kind_parameters,    only: rkind
     use neutral_pbl_concurrent_parameters
-    use constants, only: one, zero 
+    use constants, only: one, zero
     implicit none
     real(rkind), intent(out) :: Tsurf, dTsurf_dt
     character(len=*),                intent(in)    :: inputfile
-    integer :: ioUnit 
+    integer :: ioUnit
     real(rkind)  :: Lx = one, Ly = one, Lz = one, Tref = zero, Tsurf0 = one, z0init = 1.d-4, frameAngle = 0.d0
-    namelist /PROBLEM_INPUT/ Lx, Ly, Lz, Tref, Tsurf0, dTsurf_dt, z0init, frameAngle  
+    namelist /PROBLEM_INPUT/ Lx, Ly, Lz, Tref, Tsurf0, dTsurf_dt, z0init, frameAngle
     !real(rkind)  :: beta, sigma, phi_ref
     !integer :: z_ref
-    !namelist /PROBLEM_INPUT/ Lx, Ly, Lz, Tref, Tsurf0, dTsurf_dt, z0init, frameAngle!, beta, sigma, phi_ref, z_ref    
- 
+    !namelist /PROBLEM_INPUT/ Lx, Ly, Lz, Tref, Tsurf0, dTsurf_dt, z0init, frameAngle!, beta, sigma, phi_ref, z_ref
+
     ioUnit = 11
     open(unit=ioUnit, file=trim(inputfile), form='FORMATTED')
     read(unit=ioUnit, NML=PROBLEM_INPUT)
-    close(ioUnit)    
+    close(ioUnit)
 
     dTsurf_dt = dTsurf_dt /  3600.d0
 
     ! Normalize
-    dTsurf_dt = dTsurf_dt * timeDim 
+    dTsurf_dt = dTsurf_dt * timeDim
 
     Tsurf = Tsurf0
 end subroutine
@@ -187,7 +187,7 @@ subroutine hook_probes(inputfile, probe_locs)
     real(rkind), dimension(:,:), allocatable, intent(inout) :: probe_locs
     character(len=*),                intent(in)    :: inputfile
     integer, parameter :: nprobes = 2
-    
+
     ! IMPORTANT : Convention is to allocate probe_locs(3,nprobes)
     ! Example: If you have at least 3 probes:
     ! probe_locs(1,3) : x -location of the third probe
@@ -197,7 +197,7 @@ subroutine hook_probes(inputfile, probe_locs)
 
     ! Add probes here if needed
     ! Example code: The following allocates 2 probes at (0.1,0.1,0.1) and
-    ! (0.2,0.2,0.2)  
+    ! (0.2,0.2,0.2)
     allocate(probe_locs(3,nprobes))
     probe_locs(1,1) = 0.1d0; probe_locs(2,1) = 0.1d0; probe_locs(3,1) = 0.1d0;
     probe_locs(1,2) = 0.2d0; probe_locs(2,2) = 0.2d0; probe_locs(3,2) = 0.2d0;
@@ -210,7 +210,7 @@ end subroutine
 
 
 subroutine meshgen_wallM(decomp, dx, dy, dz, mesh, inputfile)
-    use neutral_pbl_concurrent_parameters    
+    use neutral_pbl_concurrent_parameters
     use kind_parameters,  only: rkind
     use constants,        only: one,two
     use decomp_2d,        only: decomp_info
@@ -224,13 +224,13 @@ subroutine meshgen_wallM(decomp, dx, dy, dz, mesh, inputfile)
     integer :: ix1, ixn, iy1, iyn, iz1, izn
     real(rkind)  :: Lx = one, Ly = one, Lz = one, Tref = zero, Tsurf0 = one, dTsurf_dt = -0.05d0, z0init = 1.d-4, frameAngle = 0.d0
     !real(rkind)  :: beta, sigma, phi_ref
-    !integer :: z_ref 
-    namelist /PROBLEM_INPUT/ Lx, Ly, Lz, Tref, Tsurf0, dTsurf_dt, z0init, frameAngle 
+    !integer :: z_ref
+    namelist /PROBLEM_INPUT/ Lx, Ly, Lz, Tref, Tsurf0, dTsurf_dt, z0init, frameAngle
 
     ioUnit = 11
     open(unit=ioUnit, file=trim(inputfile), form='FORMATTED')
     read(unit=ioUnit, NML=PROBLEM_INPUT)
-    close(ioUnit)    
+    close(ioUnit)
 
     !Lx = two*pi; Ly = two*pi; Lz = one
 
@@ -239,7 +239,7 @@ subroutine meshgen_wallM(decomp, dx, dy, dz, mesh, inputfile)
     ! If base decomposition is in Y
     ix1 = decomp%xst(1); iy1 = decomp%xst(2); iz1 = decomp%xst(3)
     ixn = decomp%xen(1); iyn = decomp%xen(2); izn = decomp%xen(3)
-    
+
     associate( x => mesh(:,:,:,1), y => mesh(:,:,:,2), z => mesh(:,:,:,3) )
 
         dx = Lx/real(nxg,rkind)
@@ -256,10 +256,10 @@ subroutine meshgen_wallM(decomp, dx, dy, dz, mesh, inputfile)
             end do
         end do
 
-        ! Shift everything to the origin 
+        ! Shift everything to the origin
         x = x - dx
         y = y - dy
-        z = z - dz 
+        z = z - dz
 
     end associate
 
@@ -267,24 +267,24 @@ end subroutine
 
 subroutine set_Reference_Temperature(inputfile, Thetaref)
     use kind_parameters,    only: rkind
-    use constants, only: one, zero 
+    use constants, only: one, zero
     implicit none
     character(len=*),                intent(in)    :: inputfile
     real(rkind), intent(out) :: Thetaref
-    integer :: ioUnit 
-    real(rkind)  :: Lx = one, Ly = one, Lz = one, Tref = zero, Tsurf0 = one, dTsurf_dt = -0.05d0, z0init = 2.5d-4, frameAngle = 0.d0 
-    namelist /PROBLEM_INPUT/ Lx, Ly, Lz, Tref, Tsurf0, dTsurf_dt, z0init, frameAngle  
+    integer :: ioUnit
+    real(rkind)  :: Lx = one, Ly = one, Lz = one, Tref = zero, Tsurf0 = one, dTsurf_dt = -0.05d0, z0init = 2.5d-4, frameAngle = 0.d0
+    namelist /PROBLEM_INPUT/ Lx, Ly, Lz, Tref, Tsurf0, dTsurf_dt, z0init, frameAngle
     !real(rkind)  :: beta, sigma, phi_ref
     !integer :: z_ref
-    !namelist /PROBLEM_INPUT/ Lx, Ly, Lz, Tref, Tsurf0, dTsurf_dt, z0init, frameAngle!, beta, sigma, phi_ref, z_ref     
+    !namelist /PROBLEM_INPUT/ Lx, Ly, Lz, Tref, Tsurf0, dTsurf_dt, z0init, frameAngle!, beta, sigma, phi_ref, z_ref
 
     ioUnit = 11
     open(unit=ioUnit, file=trim(inputfile), form='FORMATTED')
     read(unit=ioUnit, NML=PROBLEM_INPUT)
-    close(ioUnit)    
+    close(ioUnit)
 
     Thetaref = Tref
-    ! This will set the value of Tref.     
+    ! This will set the value of Tref.
 
 end subroutine
 
@@ -292,7 +292,7 @@ end subroutine
 subroutine set_KS_planes_io(planesCoarseGrid, planesFineGrid)
     integer, dimension(:), allocatable,  intent(inout) :: planesFineGrid
     integer, dimension(:), allocatable,  intent(inout) :: planesCoarseGrid
-    
+
     allocate(planesCoarseGrid(1), planesFineGrid(1))
     planesCoarseGrid = [8]
     planesFineGrid = [16]
@@ -301,12 +301,12 @@ end subroutine
 
 
 module link_turbine_to_scalar
-    use kind_parameters, only: rkind, clen 
-    implicit none 
+    use kind_parameters, only: rkind, clen
+    implicit none
     complex(rkind), dimension(:,:,:), allocatable :: rhs_x, rhs_y, rhs_z
     real(rkind), dimension(:,:,:), allocatable :: utmp, vtmp, wtmp, scalar_source_0
-    character(clen) :: OutputDir 
-contains 
+    character(clen) :: OutputDir
+contains
 
     subroutine setup_turb_scalar_source(primary)
         use decomp_2d_io
@@ -317,22 +317,22 @@ contains
         real(rkind), dimension(10000) :: inst_horz_avg
         character(len=clen) :: fname
 
-        
+
         allocate(utmp(primary%gpC%xsz(1),primary%gpC%xsz(2),primary%gpC%xsz(3)))
         allocate(vtmp(primary%gpC%xsz(1),primary%gpC%xsz(2),primary%gpC%xsz(3)))
         allocate(wtmp(primary%gpE%xsz(1),primary%gpE%xsz(2),primary%gpE%xsz(3)))
-        
+
         allocate(scalar_source_0(primary%gpC%xsz(1),primary%gpC%xsz(2),primary%gpC%xsz(3)))
         allocate(scalar_source_1(primary%gpC%xsz(1),primary%gpC%xsz(2),primary%gpC%xsz(3)))
-        
-        allocate(rhs_y(primary%sp_gpC%ysz(1), primary%sp_gpC%ysz(2), primary%sp_gpC%ysz(3))) 
-        allocate(rhs_z(primary%sp_gpE%ysz(1), primary%sp_gpE%ysz(2), primary%sp_gpE%ysz(3))) 
-       
-        utmp = 1.d0 
-        vtmp = 0.d0 
+
+        allocate(rhs_y(primary%sp_gpC%ysz(1), primary%sp_gpC%ysz(2), primary%sp_gpC%ysz(3)))
+        allocate(rhs_z(primary%sp_gpE%ysz(1), primary%sp_gpE%ysz(2), primary%sp_gpE%ysz(3)))
+
+        utmp = 1.d0
+        vtmp = 0.d0
         wtmp = 0.d0
         ! Scalar 3, primary%scalars(3)%source_hat contains spatial flags for the
-        ! points of interest 
+        ! points of interest
         call primary%spectC%ifft(primary%scalars(3)%source_hat,scalar_source_0)
         call primary%WindTurbineArr%getForceRHS( 1.d0, utmp, vtmp, wtmp, primary%scalars(1)%source_hat, rhs_y, rhs_z, .true., inst_horz_avg)
         call primary%spectC%ifft(primary%scalars(1)%source_hat,scalar_source_1)
@@ -345,18 +345,18 @@ contains
         call primary%spectC%ifft(primary%scalars(1)%source_hat,scalar_source_1)
         scalar_source_0 = -scalar_source_0 * scalar_source_1 / 1.0d4
         call primary%spectC%fft(scalar_source_0, primary%scalars(4)%source_hat)
-        ! Scalar 1 
+        ! Scalar 1
         call primary%WindTurbineArr%getForceRHS( 1.d0, utmp, vtmp, wtmp, primary%scalars(1)%source_hat, rhs_y, rhs_z, .true., inst_horz_avg)
-        primary%scalars(1)%source_hat = -primary%scalars(1)%source_hat / 1.0d4      
-  
+        primary%scalars(1)%source_hat = -primary%scalars(1)%source_hat / 1.0d4
+
         call primary%spectC%ifft(primary%scalars(1)%source_hat,scalar_source_0)
         fname = primary%OutputDir(:len_trim(primary%OutputDir))//"/ScalarSource1.out"
         call decomp_2d_write_one(1,scalar_source_0,fname,primary%gpC)
-        
+
         call primary%spectC%ifft(primary%scalars(2)%source_hat,scalar_source_0)
         fname = primary%OutputDir(:len_trim(primary%OutputDir))//"/ScalarSource2.out"
         call decomp_2d_write_one(1,scalar_source_0,fname,primary%gpC)
-        
+
         call primary%spectC%ifft(primary%scalars(3)%source_hat,scalar_source_0)
         fname = primary%OutputDir(:len_trim(primary%OutputDir))//"/ScalarSource3.out"
         call decomp_2d_write_one(1,scalar_source_0,fname,primary%gpC)
@@ -364,14 +364,14 @@ contains
         call primary%spectC%ifft(primary%scalars(4)%source_hat,scalar_source_0)
         fname = primary%OutputDir(:len_trim(primary%OutputDir))//"/ScalarSource4.out"
         call decomp_2d_write_one(1,scalar_source_0,fname,primary%gpC)
-        
+
         call message(0,"SCALAR SOURCES WRITTEN TO DISK.")
-    
+
         deallocate(utmp, vtmp, wtmp, scalar_source_0, rhs_y, rhs_z, scalar_source_1)
 
-    end subroutine 
+    end subroutine
 
-end module 
+end module
 
 
 subroutine initScalar(decompC, inpDirectory, mesh, scalar_id, scalarField)
@@ -384,7 +384,7 @@ subroutine initScalar(decompC, inpDirectory, mesh, scalar_id, scalarField)
     real(rkind), dimension(:,:,:), intent(out)     :: scalarField
 
     scalarField = 0.d0
-end subroutine 
+end subroutine
 
 subroutine setScalar_source(decompC, inpDirectory, mesh, scalar_id, scalarSource)
     use kind_parameters, only: rkind
@@ -398,6 +398,7 @@ subroutine setScalar_source(decompC, inpDirectory, mesh, scalar_id, scalarSource
     real(rkind), dimension(:,:,:), intent(out)     :: scalarSource
     real(rkind), dimension(:,:,:), pointer :: x, y, z
     real(rkind) :: dz, dy, dx, Lx, Ly
+    integer :: i, j, k
 
     z => mesh(:,:,:,3); y => mesh(:,:,:,2); x => mesh(:,:,:,1)
     dz = z(1,1,2) - z(1,1,1)
@@ -406,11 +407,11 @@ subroutine setScalar_source(decompC, inpDirectory, mesh, scalar_id, scalarSource
     Lx = 75.d0
     Ly = 35.d0
     select case (scalar_id)
-    case (1) ! Turbine case
-        scalarSource = 0.d0 ! Need to handle this case using init_turb2scalar_linker call 
-    case (2) ! Above the turbine rows
+      case (1) ! Turbine case
+        scalarSource = 0.d0 ! Need to handle this case using init_turb2scalar_linker call
+      case (2) ! Above the turbine rows
         scalarSource = 0.d0
-        scalarSource = exp(-(z-(0.25d0+1.5d0*126.d0/400.d0))**2 / 0.01) / 1.0d4 ! Mike implement this 
+        scalarSource = exp(-(z-(0.25d0+1.5d0*126.d0/400.d0))**2 / 0.01) / 1.0d4 ! Mike implement this
         do k = 1,size(z,3)
             do j = 1, size(z,2)
                 do i = 1, size(z,1)
@@ -420,9 +421,9 @@ subroutine setScalar_source(decompC, inpDirectory, mesh, scalar_id, scalarSource
                 end do
             end do
         end do
-    case (3) ! Center turbinethis%blanks = 1.d0
+      case (3) ! Center turbinethis%blanks = 1.d0
         scalarSource = z*0.d0
-        scalarSource = 1.d0 ! Need to handle this case using init_turb2scalar_linker call 
+        scalarSource = 1.d0 ! Need to handle this case using init_turb2scalar_linker call
         do k = 1,size(z,3)
             do j = 1, size(y,2)
                 do i = 1,size(x,1)
@@ -435,9 +436,9 @@ subroutine setScalar_source(decompC, inpDirectory, mesh, scalar_id, scalarSource
                 end do
             end do
         end do
-    case (4) ! Last turbine
+      case (4) ! Last turbine
         scalarSource = z*0.d0
-        scalarSource = 1.d0 ! Need to handle this case using init_turb2scalar_linker call 
+        scalarSource = 1.d0 ! Need to handle this case using init_turb2scalar_linker call
         do k = 1,size(z,3)
             do j = 1, size(y,2)
                 do i = 1,size(x,1)
@@ -450,12 +451,12 @@ subroutine setScalar_source(decompC, inpDirectory, mesh, scalar_id, scalarSource
                 end do
             end do
         end do
-    !case (XXX) ! Turbine case
-    !    scalarSource = 0.d0 ! Need to handle this case using init_turb2scalar_linker call 
-    !    scalarSource = 0.d0 ! Need to handle this case using init_turb2scalar_linker call 
-    !    scalarSource = z*0.d0
-    !    scalarSource(1,:,:) = exp(-(z(1,:,:)-(dz))**2 / 0.01) ! Mike implement this 
-    !    scalarSource(:,1,:) = exp(-(z(:,1,:)-(dz))**2 / 0.01) ! Mike implement this 
+        !case (XXX) ! Turbine case
+        !    scalarSource = 0.d0 ! Need to handle this case using init_turb2scalar_linker call
+        !    scalarSource = 0.d0 ! Need to handle this case using init_turb2scalar_linker call
+        !    scalarSource = z*0.d0
+        !    scalarSource(1,:,:) = exp(-(z(1,:,:)-(dz))**2 / 0.01) ! Mike implement this
+        !    scalarSource(:,1,:) = exp(-(z(:,1,:)-(dz))**2 / 0.01) ! Mike implement this
     end select
 
-end subroutine 
+end subroutine
