01e7d2e778677c3ff4dbe8b53f1d326fa30edbc5
Skylar Gering <sgering@mit.edu>
2025-03-20 12:40:32 -0500

    Add stampeded setup file

---
+++ setup/Setup_Stampede3.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+module load cmake
+module load intel impi
+module load fftw3/3.3.10
+
+CWD=`pwd`
+export COMPILER_ID=Intel
+export FC=mpiifort
+export CC=mpiicc
+export CXX=mpiicpc
+export FFTW_PATH=$TACC_FFTW3_DIR
+export DECOMP_PATH=${CWD}/dependencies/2decomp_fft
+export VTK_IO_PATH=${CWD}/dependencies/Lib_VTK_IO/build
+export HDF5_PATH=${CWD}/dependencies/hdf5-1.14.3/build
+export FFTPACK_PATH=${CWD}/dependencies/fftpack
+export ARCH_OPT_FLAG="-xCORE-AVX512"
