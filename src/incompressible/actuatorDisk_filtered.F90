8187f62b75352ae7036ade05df99c75bb8305ce6
Kirby Heck <kirby.heck@gmail.com>
2025-08-13 13:58:09 -0500

    new, fast implementation for ADM type 5 (TESTED)
    Rebase to latest igridSGS-KSH

--- src/incompressible/actuatorDisk_filtered.F90
+++ src/incompressible/actuatorDisk_filtered.F90
@@ -240,8 +240,11 @@ end subroutine
 subroutine get_R(this)
     class(actuatordisk_filtered), intent(inout) :: this
     real(rkind) :: yrad, trad, xs, ys, zs, C1, xtmp, ytmp, ztmp  ! rotations, in radians
-    real(rkind), dimension(int(this%npts)) :: xi, yi, zi
-    integer :: k
+    real(rkind), dimension(this%npts) :: xi, yi, zi
+    ! integer :: k
+    real(rkind) :: rcut, coef, rsq
+    real(rkind) :: xmin, xmax, ymin, ymax, zmin, zmax
+    integer :: i1, i2, j1, j2, k1, k2, i, j, k, l
     
     ! First, rotate all the points with the yaw and tilt
     ! call message(1, "Building kernel for turbine yaw:", this%yaw)
@@ -261,13 +264,48 @@ subroutine get_R(this)
     end do
     
     ! now xi, yi, zi are the rotated coordinates, assemble w/Greens function 
-    ! this may take a while... 
-    ! TODO: can speed this up if only a subsection of the domain is used
-    C1 = (6.d0/pi/this%delta**2)**(three/two)
-    ! TODO: May need to zero scalarsource for dynamic yaw
-    do k = 1, int(this%npts)
-        this%rbuff = (this%xG-xi(k))**2 + (this%yG-yi(k))**2 + (this%zG-zi(k))**2
-        this%scalarsource = this%scalarsource + C1*exp(-6.d0*this%rbuff/this%delta**2) 
+
+    ! Slow implementation
+    ! C1 = (6.d0/pi/this%delta**2)**(three/two)
+    ! do k = 1, this%npts
+    !     this%rbuff = (this%xG-xi(k))**2 + (this%yG-yi(k))**2 + (this%zG-zi(k))**2
+    !     this%scalarsource = this%scalarsource + C1*exp(-6.d0*this%rbuff/this%delta**2) 
+    ! end do
+
+    ! faster implementation: 
+    rcut   = 2.d0 * this%delta  ! this includes >99.999% of the forcing
+    coef   = -6.d0 / this%delta**2
+    C1     = (6.d0/pi/this%delta**2)**(three/two)
+
+    do k = 1, this%npts
+        ! bounds in physical space
+        xmin = xi(k) - rcut
+        xmax = xi(k) + rcut
+        ymin = yi(k) - rcut
+        ymax = yi(k) + rcut
+        zmin = zi(k) - rcut
+        zmax = zi(k) + rcut
+
+        ! find index limits (assuming monotonic coordinates in each direction)
+        ! Using max/min to clip to local array bounds
+        i1 = max(1, minloc(abs(this%xG(:,1,1) - xmin), dim=1))
+        i2 = min(this%nxLoc, minloc(abs(this%xG(:,1,1) - xmax), dim=1))
+        j1 = max(1, minloc(abs(this%yG(1,:,1) - ymin), dim=1))
+        j2 = min(this%nyLoc, minloc(abs(this%yG(1,:,1) - ymax), dim=1))
+        k1 = max(1, minloc(abs(this%zG(1,1,:) - zmin), dim=1))
+        k2 = min(this%nzLoc, minloc(abs(this%zG(1,1,:) - zmax), dim=1))
+
+        ! loop only over the small cube around the point
+        do l = k1, k2
+            do j = j1, j2
+                do i = i1, i2
+                    rsq = (this%xG(i,j,l) - xi(k))**2 + &
+                                        (this%yG(i,j,l) - yi(k))**2 + &
+                                        (this%zG(i,j,l) - zi(k))**2
+                    this%scalarsource(i,j,l) = this%scalarsource(i,j,l) + C1 * exp(coef * rsq)
+                end do
+            end do
+        end do
     end do
     
     ! scalarsource NOT necessarily normalized to integrate to 1 (yet), do this in get_weights()
