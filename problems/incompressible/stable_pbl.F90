a174c5753bc7cf8b18c3f37a6bc8d87b7e9796c8
Kirby Heck <kirby.heck@gmail.com>
2025-08-18 16:11:03 -0500

    Initial SBL problems copied from gabls_igrid

---
+++ problems/incompressible/stable_pbl.F90
@@ -0,0 +1,58 @@
+! Stable boundary layer simulation, modified from gabls_igrid.F90
+! and from stable_pbl on igridSGS-KSK
+
+#include "stable_pbl_files/initialize.F90"
+#include "stable_pbl_files/temporalHook.F90"
+
+program stable_pbl
+    use mpi
+    use kind_parameters,  only: clen
+    use IncompressibleGrid, only: igrid
+    use temporalhook, only: doTemporalStuff, initialize_controller_location
+    use timer, only: tic, toc
+    use budgets_xy_avg_mod, only: budgets_xy_avg
+    use budgets_time_avg_mod, only: budgets_time_avg
+    use exits, only: message
+
+    implicit none
+
+    type(igrid), allocatable, target :: igp
+    character(len=clen) :: inputfile
+    integer :: ierr
+    type(budgets_xy_avg) :: budg_xy
+    type(budgets_time_avg) :: budg_tavg
+
+    call MPI_Init(ierr)                     !<-- Begin MPI
+
+    call GETARG(1,inputfile)                !<-- Get the location of the input file
+
+    call compute_xdim_udim(inputfile)       !<-- Reads the \PHYSICS\ namelist to compute (xdim, udim) from (Ro, Fr)
+
+    allocate(igp)                           !<-- Initialize hit_grid with defaults
+    call igp%init(inputfile)                !<-- Properly initialize the hit_grid solver (see hit_grid.F90)
+    call igp%start_io(.true.)               !<-- Start I/O by creating a header file (see io.F90)
+    call igp%printDivergence()
+
+    call budg_xy%init(inputfile, igp)       !<-- Budget class initialization
+    call budg_tavg%init(inputfile, igp)     !<-- Budget class initialization
+
+    call tic()
+    do while (igp%tsim < igp%tstop)
+
+        call igp%timeAdvance()              !<-- Time stepping scheme + Pressure Proj. (see igridWallM.F90)
+        call doTemporalStuff(igp)           !<-- Go to the temporal hook (see temporalHook.F90)
+
+        call budg_xy%doBudgets()            !<--- perform budget related operations
+        call budg_tavg%doBudgets()          !<--- perform budget related operations
+    end do
+
+    call budg_xy%destroy()                  !<-- release memory taken by the budget class
+    call budg_tavg%destroy()                !<-- release memory taken by the budget class
+
+    call igp%finalize_io()                  !<-- Close the header file (wrap up i/o)
+    call igp%destroy()                      !<-- Destroy the IGRID derived type
+    deallocate(igp)                         !<-- Deallocate all the memory associated with scalar defaults
+
+    call MPI_Finalize(ierr)                 !<-- Terminate MPI
+
+end program
