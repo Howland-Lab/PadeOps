35ef5d176a524d4c2ce47ef0d3bd4402d76d8014
karimali5 <karimalib45@gmail.com>
2026-08-15 20:06:30 -0400

    switch to local hdf5 for gcc anvil

--- setup/SetupEnv_Anvil_gcc.sh
+++ setup/SetupEnv_Anvil_gcc.sh
@@ -4,7 +4,7 @@ module load gcc/14.2.0
 module load openmpi/5.0.5
 module load openblas/0.3.27
 module load fftw/3.3.8 
-module load hdf5/1.10.7
+module load libszip/2.1.1
 module load cmake/3.20.0
 module list 
 
@@ -13,7 +13,7 @@ export FC=mpif90
 export CC=mpicc
 export CXX=mpicpc
 export FFTW_PATH=${FFTW_HOME}
-export HDF5_PATH=${HDF5_HOME}
+export HDF5_PATH=/anvil/projects/x-atm170028/karim/PadeOps/dependencies/gcc/hdf5-1.14.3/install
 export DECOMP_PATH=/anvil/projects/x-atm170028/karim/PadeOps/dependencies/gcc/2decomp_fft
 export FFTPACK_PATH=dummy
 export ARCH_OPT_FLAG="-march=znver3"
